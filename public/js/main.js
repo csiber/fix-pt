0e26f1c6ef28796fcd6def39dbf27625ae018607
unknown <c_d_l_c@hotmail.com>
2013-11-23 16:39:46 +0000

    adicionado botao de favoritos

--- public/js/main.js
+++ public/js/main.js
@@ -21,3 +21,13 @@ $(document).ready(function(){
 window.addEventListener('load', function() {
     new FastClick(document.body);
 }, false);
+
+function markFixerAsFavorite(star) {
+	star = $(star);
+	if(star.hasClass('glyphicon-star-empty')) {
+		star.removeClass('glyphicon-star-empty').addClass('glyphicon-star');
+	}
+ 	else {
+ 		star.removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+ 	}
+}
