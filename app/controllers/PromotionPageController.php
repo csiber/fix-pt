66458c7d9d96308a39404ff12212b735eff1ba54
cdlc <c_d_l_c@hotmail.com>
2013-12-19 23:07:26 +0000

    changes

--- app/controllers/PromotionPageController.php
+++ app/controllers/PromotionPageController.php
@@ -40,10 +40,12 @@ class PromotionPageController extends BaseController {
     public function getShow($id)
     {
         $promotionpage = PromotionPage::getPromotionPage($id);
-        $isFavorite = Favorite::checkFavorite($id);
+        $user2 = $promotionpage->post->user->id;
+        $user1 = Auth::user()->id;
+        $isFavorite = Favorite::checkFavorite($user1, $user2);
         return View::make('promotionpages.show',
             array('promotionpage' => $promotionpage,
-            "favorite"=> $isFavorite)
+            'favorite' => $isFavorite)
         );
     }
 
